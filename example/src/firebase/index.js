743a62bfacc164c035224bd8911d465f5b7a9b6b
tutr-PC\tutrieuchau <nguyenductamwind@gmail.com>
2017-12-15 23:55:58 +0700

    update template

--- example/src/firebase/index.js
+++ example/src/firebase/index.js
@@ -4,19 +4,9 @@ var serviceAccount = require("../../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-54451.firebaseio.com"
+  databaseURL: "https://pikerfree.firebaseio.com"
 });
 const db = admin.database();
-const getAllFireBaseData = () => {
-  let ref = db.ref("users");
-  var usersRef = ref.child('users');
-  // Attach an asynchronous callback to read the data at our posts reference
-  ref.on("value", function (snapshot) {
-    console.log(snapshot.val());
-  }, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
-}
 const getAllUsers = async() => {
   let ref = db.ref("users");
   return new Promise((resolve,reject) => {
@@ -31,7 +21,30 @@ const addUser = (user) => {
   var usersRef = ref.child('users');
   usersRef.set(user);
 }
+const getAllPost = () => {
+  let ref = db.ref("posts");
+  return new Promise((resolve,reject) => {
+    ref.on('value', snapshot => {
+        resolve(snapshot.val() );
+        console.log(snapshot.val());
+    }, err => {
+      reject( err );
+      console.log(err);
+    } );
+  });
+}
+const getUserByUserId = (userId) =>{
+  let ref = db.ref("users/"+userId);
+  return new Promise((resolve,reject) => {
+    ref.once('value', snapshot => {
+        resolve(snapshot.val() );
+    }, err => {
+      reject( err );
+    } );
+  });
+}
 module.exports = {
-  getAllFireBaseData,
-  getAllUsers
+  getAllUsers,
+  getUserByUserId,
+  getAllPost
 };
--- example/src/routes/post.js
+++ example/src/routes/post.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const firebase = require('../firebase');
 router.get('/',async (req, res) => {
     // firebase.getAllFireBaseData();
-    let users = await firebase.getAllUsers();
-    console.log(users);
+    let posts = await firebase.getAllPost();
+    console.log(posts);
+    res.render('post');
 });
 module.exports = router;

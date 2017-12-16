2d2a5cb192f103205fea88e491427f1268adffb8
tutr-PC\tutrieuchau <nguyenductamwind@gmail.com>
2017-12-17 02:52:49 +0700

    'lap'

--- example/src/routes/user.js
+++ example/src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const firebase = require('../firebase');
+let AVATAR = ['F696gEJCteXys5VY88PAv5la7N33.jpg','kdFQ8xxaCjeganYpVVUFBUW4FOC3.jpg','NnwjOc1LhMeVZVuNY1tj9Nod8Lv1.jpg','pTfN1xcgJzfLgMgs5jX3weaBLuq1.jpg','wdsGD6HWcIXarvHc6N68f7Mz8hc2.jpg','yNzfHV6o7qOi9VkDbuid1S0a9Gp1.jpg'];
 router.get('/',async (req, res) => {
     let users = await firebase.getAllUsers();
     /** Convert users object to array */
@@ -11,13 +12,19 @@ router.get('/',async (req, res) => {
     res.render('user',{users:userArray});
 });
 
-router.get('/:id', (req,res) => {
-    var user = {name:'',email:'',slogan:'',address:'',phoneNo:''}
-    res.render('main',{user:user,type:'add'});
-});
-
-router.get('/add',(req,res)=>{
-    var user = {name:'',email:'',slogan:'',address:'',phoneNo:''}
-    res.render('profile',{user:user,type:'add'});
+router.get('/:id',async (req,res) => {
+    let userId = req.params.id;
+    let users = await firebase.getAllUsers();
+    let posts = await firebase.getAllPost();
+    let user = users[userId];
+    if(userId == 'add' || !user){
+        user = {name:'',email:'',slogan:'',address:'',phoneNo:''}
+        res.render('profile',{user:user,type:'add'});
+    }else{
+        if(AVATAR.indexOf(user.avatarLink.replace('userImages/','')) == -1){
+            user.avatarLink = 'userImages/default_profile.jpg';
+        }
+        res.render('profile',{user:user,type:'edit'});
+    }
 });
 module.exports = router;
